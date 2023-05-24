07fbc8bc020b0875a96f48d20f9c2350d6660d48
Andramao <111099632+Andramao@users.noreply.github.com>
2023-05-23 21:12:03 -0500

    proyecto aluraFood version 1

---
+++ src/Form/DatosEntrega/validaciones.js
@@ -0,0 +1,15 @@
+export const validarInput = (input) => {
+    return input.length > 4 ? true : false;    
+};
+
+/*export const validarCiudad = (city) => {
+    const length = apellidos.length;
+    return length > 1 && length < 50 ? true : false;   
+};
+
+
+export const validarProvincia = (province) => {
+    const length = telefono.length
+    return  length >= 8 && length <= 14 ? true : false    
+};*/
+
